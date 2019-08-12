ebfebde2300d9ebda7c1720e00370f340ec4594f
Soniawangui <koisonia99@gmail.com>
2019-08-12 23:46:44 +0300

    Create a template file that will display search results

--- hood/urls.py
+++ hood/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
   url('^$',views.welcome,name= 'welcome'),
-  url(r'^search/', views.search_results, name='search_results')
+  url(r'^search/', views.search_business, name='search')
 ]
