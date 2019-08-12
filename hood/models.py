2bc84f24915e3b20b8eee1f7780d180c15151a60
Soniawangui <koisonia99@gmail.com>
2019-08-12 14:49:12 +0300

    Create a profile class which will be the users profile

--- hood/models.py
+++ hood/models.py
@@ -7,3 +7,11 @@ class Neighbourhood(models.Model):
   members=models.IntegerField()
   hood_image=models.ImageField(upload_to='hood/')
 
+class Profile(models.Model):
+  name=models.CharField(max_length=60)
+  user=models.ForeignKey(User,on_delete=models.CASCADE)
+  neighbourhood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+  email=models.CharField(max_length=100)
+  prof_pic=models.ImageField(upload_to='profiles/')
+
+
