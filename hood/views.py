cd8085e79414353fe142de57df22118fe0f216c2
Soniawangui <koisonia99@gmail.com>
2019-08-12 22:47:26 +0300

    Create a login_required decorator which will give access to only authenticated users

--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
   title = 'hood'
   return render(request, 'welcome.html',{"title":title})
+
+@login_required(login_url='/accounts/login')
+def hoods(request,neighbourhood_id): 
