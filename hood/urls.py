8e4c237a143eba5f1ff9fd8f82aad186e3d754c7
Soniawangui <koisonia99@gmail.com>
2019-08-13 17:11:42 +0300

    Create a profile template which will help users create profiles

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
   url('^$',views.welcome,name= 'welcome'),
@@ -8,4 +9,4 @@ urlpatterns=[
 ]
 
 if settings.DEBUG:
-  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+  urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
